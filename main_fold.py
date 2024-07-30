40c2a17183de0b6b0b3f532cc89b26da72583c37
Shreyas S <96978606+Zhreyu@users.noreply.github.com>
2024-07-30 22:31:16 +0530

    chore: Update MASTER_PORT to '5554' for distributed setup

--- main_fold.py
+++ main_fold.py
@@ -21,7 +21,7 @@ import torch.distributed as dist
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '5554'
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def cleanup():
