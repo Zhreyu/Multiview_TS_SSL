5cfc68f1ee46c7863af017dfdbdce1e4e0affb06
Shreyas S <96978606+Zhreyu@users.noreply.github.com>
2024-07-31 06:53:35 +0530

    chore: Add setup function for distributed training

--- main_fold.py
+++ main_fold.py
@@ -19,7 +19,9 @@ os.environ['WANDB_DISABLED'] = 'true'
 local_rank = int(os.environ["LOCAL_RANK"])
 import torch.distributed as dist
 
-
+def setup(rank, world_size):
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
 
 def cleanup():
     dist.destroy_process_group()
