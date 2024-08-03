c2d0e186106e4cb2e1c5cd38f3b9404a7f143e9f
Shreyas S <96978606+Zhreyu@users.noreply.github.com>
2024-08-03 22:22:25 +0530

    Refactor code to load pretrained model from specified path and fix print statement for validation subject path in main.py

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def main(args):
     if args.pretrain:
         root_path = args.root_path
         filenames = read_threshold_sub(args.sub_list)
-        filenames = [os.path.join(root_path, f) for f in filenames]
+        filenames = [os.path.join(root_path, f) for f in filenames]\
+        print(filenames)
         print('Number of subjects loaded:', len(filenames))
                 
         pretrain_dataset = IEEGDataset(
